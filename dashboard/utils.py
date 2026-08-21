c586b799ecc3f741c254b0ae766889ee1a42de5f
Awet Tesfai <tesfaiawet506@gmail.com>
2026-08-20 17:12:36 -0700

    Fetch AWS credentials from Streamlit secrets
    Added functionality to fetch AWS credentials from Streamlit secrets or environment variables.

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -3,6 +3,11 @@ from pyathena import connect
 import pandas as pd
 import streamlit as st
 
+# Fetch credentials from Streamlit Secrets (or fallback to environment variables)
+aws_access_key = st.secrets.get("AWS_ACCESS_KEY_ID", os.getenv("AWS_ACCESS_KEY_ID"))
+aws_secret_key = st.secrets.get("AWS_SECRET_ACCESS_KEY", os.getenv("AWS_SECRET_ACCESS_KEY"))
+aws_region = st.secrets.get("AWS_DEFAULT_REGION", os.getenv("AWS_DEFAULT_REGION", "us-east-1"))
+
 # Declare Variables for the AWS Region, Database name, and S3 bucket file path
 REGION = "us-east-1"
 DATABASE = "weather-pipeline-2026-db"
@@ -16,6 +21,8 @@ def run_query(query):
     # Connect is used to connect to the AWS server 
     # and fetch the table from the database
     conn = connect(
+        aws_access_key_id = aws_access_key,
+        aws_secret_access_key = aws_secret_key,
         region_name = REGION,
         s3_staging_dir = S3_STAGING_DIR,
         schema_name = DATABASE
